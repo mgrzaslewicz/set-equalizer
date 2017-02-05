08d4266d820aa05b8b425847a7cb4e8c08c53bbc
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2017-02-04 22:41:50 -0800

    Init maven project

---
+++ src/main/java/ebsystem/set/equalizer/EqualizerStart.java
@@ -0,0 +1,7 @@
+package ebsystem.set.equalizer;
+
+public class EqualizerStart {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ebsystem/set/equalizer/EqualizerTest.java
@@ -0,0 +1,14 @@
+package ebsystem.set.equalizer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EqualizerTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}

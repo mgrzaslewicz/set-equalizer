aefc9e17dfb0bdc42ba4b604bd517eeb2217677f
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-10-18 23:07:39 +0200

    Migrate to junit 5

--- src/test/java/ebsystem/set/equalizer/BestMoveFinderTest.java
+++ src/test/java/ebsystem/set/equalizer/BestMoveFinderTest.java
@@ -1,6 +1,7 @@
 package ebsystem.set.equalizer;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
--- src/test/java/ebsystem/set/equalizer/MoveTest.java
+++ src/test/java/ebsystem/set/equalizer/MoveTest.java
@@ -1,7 +1,6 @@
 package ebsystem.set.equalizer;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -9,21 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoveTest {
 
-    private final List<Integer> listA = List.of(3, 12, 5);
-    private final List<Integer> listB = List.of(10, 1, 2, 5, 9);
-    private final List<Integer> expectedElementsAfterMove = List.of(10, 1, 2, 5, 9, 3);
-
-    private Move move;
-
-    @Before
-    public void setUp() {
-        move = new Move(listA, 0, listB);
-    }
-
     @Test
     public void shouldMoveElementBetweenLists() {
+        // given
+        var listA = List.of(3, 12, 5);
+        var listB = List.of(10, 1, 2, 5, 9);
+        var move = new Move(listA, 0, listB);
+        // when
         Move.ListsAfterMove listsAfterMove = move.getListAfterMove();
-        assertThat(listsAfterMove.getListFrom()).containsExactlyElementsOf(expectedElementsAfterMove);
+        // then
+        assertThat(listsAfterMove.getListFrom()).containsExactly(10, 1, 2, 5, 9, 3);
     }
 
 }

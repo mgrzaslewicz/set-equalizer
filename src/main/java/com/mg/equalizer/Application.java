bae2500638358778addf060337bbb07c797aae36
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-10-22 00:24:10 +0200

    Add reading/writing lists from/to csv file

---
+++ src/main/java/com/mg/equalizer/Application.java
@@ -0,0 +1,60 @@
+package com.mg.equalizer;
+
+import org.slf4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class Application {
+
+    private static Logger logger = getLogger(Application.class);
+
+    private static List<List<Integer>> loadCsv(String csvFile) throws IOException {
+        List<List<Integer>> csvData = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                List<Integer> row = parseLine(line);
+                csvData.add(row);
+            }
+        }
+        return csvData;
+    }
+
+    private static List<Integer> parseLine(String line) {
+        String[] parts = line.split(",");
+        return Arrays.stream(parts)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static void saveCsv(List<List<Integer>> data, String csvFile) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
+            for (List<Integer> row : data) {
+                String line = row.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(","));
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var inputCsv = args[0];
+        var outputCsv = args[1];
+
+        var lists = loadCsv(inputCsv);
+
+        var equalizer = new Equalizer(BestMoveFinder.newBuilder().build());
+        var moves = equalizer.equalize(lists);
+
+        logger.info("Moves count: {}", moves.size());
+        saveCsv(lists, outputCsv);
+    }
+}
--- src/main/java/com/mg/equalizer/EqualizerStart.java
+++
@@ -1,7 +0,0 @@
-package com.mg.equalizer;
-
-public class EqualizerStart {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}

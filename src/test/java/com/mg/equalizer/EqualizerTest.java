13c93038bb93c98fae09682a8dfd78ee7fa6c695
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-10-21 13:13:12 +0200

    Use more complicated example in unit test

--- src/test/java/com/mg/equalizer/EqualizerTest.java
+++ src/test/java/com/mg/equalizer/EqualizerTest.java
@@ -12,16 +12,17 @@ public class EqualizerTest {
     public void shouldEqualize() {
         // given
         var equalizer = new Equalizer(new BestMoveFinder(new ImprovingMoveFinder()));
-        var list1 = new ArrayList(List.of(1, 2, 3)); // sum = 6
-        var list2 = new ArrayList(List.of(4, 5, 6)); // sum = 15
-        var list3 = new ArrayList(List.of(7, 8, 9)); // sum = 24
+        var list1 = new ArrayList(List.of(1, 2, 3, 8)); // sum = 14
+        var list2 = new ArrayList(List.of(4, 5, 6, 8)); // sum = 23
+        var list3 = new ArrayList(List.of(7, 8, 9, 3, 4)); // sum = 31
         // when
         equalizer.equalize(List.of(list1, list2, list3));
         // then
         SoftAssertions.assertSoftly(it -> {
-            it.assertThat(list1).containsExactly(1, 2, 3, 9);
-            it.assertThat(list2).containsExactly(4, 5, 6);
-            it.assertThat(list3).containsExactly(7, 8);
+            it.assertThat(list1).containsExactly(2, 3, 8, 6, 3);
+            it.assertThat(list2).containsExactly(4, 5, 8, 4, 1);
+            it.assertThat(list3).containsExactly(7, 8, 9);
         });
     }
+
 }
